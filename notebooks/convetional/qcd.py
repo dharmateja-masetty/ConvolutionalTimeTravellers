b02f7d4231d2c3c387cd427c54ff88c178914118
Mac n Tej <kosal@Mac-n-Tej.fritz.box>
2024-08-13 09:59:53 +0200

    Baseline but very bad values

---
+++ notebooks/convetional/qcd.py
@@ -0,0 +1,8 @@
+import numpy as nup
+def qcd_variance(series:any):
+        window=12
+        variances = series.rolling(window).var().dropna()
+        Q1 = nup.percentile(variances, 25, interpolation='midpoint')
+        Q3 = nup.percentile(variances, 75, interpolation='midpoint')
+        qcd = round((Q3 - Q1)/(Q3 + Q1),6)
+        return (qcd)
